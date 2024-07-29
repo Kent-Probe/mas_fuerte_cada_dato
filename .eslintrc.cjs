9b79a4d243cf76f22c4cbec937c4bf895bf62192
Kent-Probe <kevinshe01@gmail.com>
2024-07-28 20:12:41 -0500

    mas_fuerte_cada_dato: first-config

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+const RULE = {
+   OFF: "off",
+   WARN: "warn",
+   ERROR: "error",
+}
+
+module.exports = {
+   root: true,
+   env: { browser: true, es2020: true },
+   extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+      "standard",
+      "prettier",
+   ],
+   ignorePatterns: ["dist", ".eslintrc.cjs"],
+   parser: "@typescript-eslint/parser",
+   plugins: ["react-refresh"],
+   rules: {
+      "react-refresh/only-export-components": [
+         RULE.WARN,
+         { allowConstantExport: true },
+      ],
+   },
+}
