52f356af9061a9ba047c9f199067a09eefff5d79
Kent-Probe <kevinshe01@gmail.com>
2024-07-28 20:26:11 -0500

    first code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
-import Dashboard from './dasboard/dashboard';
-import Menu from './ui/components/menu';
+import Dashboard from "./dasboard/dashboard"
+import Menu from "./ui/components/menu"
 
-function App () {
-  const [s, sets] = useState();
-  
-  return (
-    <main className='w-full h-screen grid grid-cols-2'>
-        <Menu />
-        <Dashboard />
-    </main>
-  )
+function App() {
+   return (
+      <main className="w-full h-screen grid grid-cols-2">
+         <Menu />
+         <Dashboard />
+      </main>
+   )
 }
 
 export default App
